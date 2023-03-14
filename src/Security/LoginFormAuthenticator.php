265237114ce71013943717884dcbf3dea4441fd3
FlowerPink <guinguand.camille@orange.fr>
2023-03-14 12:22:13 +0100

    regex erreur inscription utilisateur

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -15,6 +15,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
+
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
@@ -23,6 +24,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function __construct(private UrlGeneratorInterface $urlGenerator)
     {
+        //ajout de cette ligne
+        $this->urlGenerator = $urlGenerator;
     }
 
     public function authenticate(Request $request): Passport
